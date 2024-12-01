a253734a3011397551383a0e931c8e603e8d5fb1
sana <sana.benslama@yahoo.com>
2024-12-01 20:43:06 +0100

    fix url

--- back-end/server.js
+++ back-end/server.js
@@ -1,13 +1,13 @@
-const API_URL ="https://test-1-qj8j.onrender.com/api"
 const PORT = process.env.PORT || 5000;
 const express = require('express')
 const app = express()
 
 const cors = require('cors');
 
-app.get(`${API_URL}/users`,(req,res)=>{
-    res.json({"users":["sana","alex","Sam"]})
-})
+app.get("/users", (req, res) => {
+    res.json({ "users": ["sana", "alex", "Sam"] });
+});
+
 
 
 app.listen(PORT, () => {
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { useEffect,useState } from 'react'
-const API_URL = process.env.REACT_APP_API_URL;
+
 
 function App() {
   const [backEndData, setBackEndData] = useState({ users: [] });
 
 
   useEffect(()=>{
-    fetch("`${API_URL}`/users").then(
+    fetch("/users").then(
       response => response.json()
     ).then(
       data => {
